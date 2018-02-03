2f0fbd705e8df5b9aada4e5879cf9baf8e15b53b
khmy2010 <kehan.see@gmail.com>
2018-02-03 20:41:07 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    console.log('Time now: ', now);
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening at port 3000.');
+})
