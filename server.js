d3b1b506cd4a19ea803ce2108a2d6f7b36a085d3
khmy2010 <kehan.see@gmail.com>
2018-02-04 08:18:48 +0800

    Added projects page

--- server.js
+++ server.js
@@ -8,26 +8,25 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    console.log('Time now: ', now);
-    next();
-});
-
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my website',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Portfolio Page',
+        welcomeMessage: 'My project portfolio is here!'
     });
 });
 
